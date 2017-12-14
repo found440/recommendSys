e56dec95e6dd9e87c45ddf009424fc23198ade18
myd <1353795848@qq.com>
2017-12-14 16:49:03 +0800

    内存不够，暂时无法完成传统knn的方法

--- test.py
+++ test.py
@@ -1,10 +1,19 @@
 # -*- coding: utf-8 -*-
+#猜你喜欢，http://www.dcjingsai.com/common/cmpt/%E7%8C%9C%E4%BD%A0%E5%96%9C%E6%AC%A2_%E7%AB%9E%E8%B5%9B%E4%BF%A1%E6%81%AF.html
+
 import numpy as np
 import pandas as pd
 
 trainData = pd.read_csv('../data/train.csv')
 testData = pd.read_csv('../data/test.csv')
+maxUid = trainData['uid'].max()
+maxIid = trainData['iid'].max()
+#print trainData['uid'].max()
+#print trainData['iid'].max()
+#print trainData.groupby('uid').count()  #uid数量157949,最大值是223969
+#print trainData.groupby('iid').count()  #iid数量14620,最大值是14725
 
+#方法1：平均分
 #先计算一次用户及物品的平均分, 最终成绩7.82
 def getMean():
     rateRank = trainData.groupby('uid').mean().loc[:, ['score']].iloc[:, -1]
@@ -21,9 +30,20 @@ def getMean():
 def generateRes(result, filename):
     result.loc[:, ['score']].to_csv(filename + '.csv', index=False)
 
+#由于在构建评分矩阵的时候出现了MemoryError，想想也是，223969*14725*32bit/(8*1024*1024*1024) = 12.28GB，Boom
+def generateRateMatCsv():
+    rateMat = np.zeros((maxUid+1, maxIid+1))
+    for (uid, iid, score, time) in trainData:
+        rateMat[uid, iid] = score
+    np.savetxt('rate_mat.csv', rateMat, delimiter = ',') 
+
+
+
 #传统的协同过滤方法，计算pearson相似度，欧氏距离，余弦相似度等
 def pearson():
     pass
 
 if __name__ == '__main__':
-    getMean()
+    #generateRateMatCsv();
+    #getMean()
+    pass
