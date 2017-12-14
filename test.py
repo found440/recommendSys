50cd17b6059c5a5aeb344f2410d0360b33a7b870
myd <1353795848@qq.com>
2017-12-14 09:31:33 +0800

    DataCastle猜你喜欢 http://www.dcjingsai.com/common/cmpt/%E7%8C%9C%E4%BD%A0%E5%96%9C%E6%AC%A2_%E7%AB%9E%E8%B5%9B%E4%BF%A1%E6%81%AF.html

---
+++ test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+
+trainData = pd.read_csv("../data/train.csv")
+testData = pd.read_csv("../data/test.csv")
+print train_data[0]
+
+#传统的knn模型由于太耗费内存
+def pearson():
+    pass
+
+if __name__ == '__main__':
+    pass
