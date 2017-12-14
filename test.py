527ff269d6d6ec22054f07eda3bb42994ec1b296
myd <1353795848@qq.com>
2017-12-14 10:34:38 +0800

    完成计算用户及物品平均分

--- test.py
+++ test.py
@@ -2,13 +2,25 @@
 import numpy as np
 import pandas as pd
 
-trainData = pd.read_csv("../data/train.csv")
-testData = pd.read_csv("../data/test.csv")
-print train_data[0]
+trainData = pd.read_csv('../data/train.csv')
+testData = pd.read_csv('../data/test.csv')
 
-#传统的knn模型由于太耗费内存
+#先计算一次用户及物品的平均分
+def getMean():
+    rateRank = trainData.groupby('uid').mean().loc[:, ['score']].iloc[:, -1]
+    rateRank = pd.DataFrame(np.int32((rateRank *2).values), index=rateRank.index, columns=['group'])
+    rateRankDes = rateRank.reset_index()
+
+    trainPlus = pd.merge(trainData, rateRankDes, how='left', on='uid')
+    testPlus = pd.merge(testData, rateRankDes, how='left', on='uid')
+    res = trainPlus.groupby(['iid', 'group']).mean().reset_index().loc[:, ['iid', 'group', 'score']]
+    result = pd.merge(testPlus, res, how='left', on=['iid', 'group']).fillna(3.0)
+    #result.loc[:, ['uid', 'iid', 'score']].to_csv('meanResult.csv', index=False)
+    return res
+
+#传统的协同过滤方法，计算pearson相似度，欧氏距离，余弦相似度等
 def pearson():
-    pass
+    
 
 if __name__ == '__main__':
-    pass
+    getMean()
